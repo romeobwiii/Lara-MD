e917e65eeaade74460cda5594ef34cc857cd9feb
Romeo bw iii <161124347+romeobwiii@users.noreply.github.com>
2024-12-08 12:15:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "2uBAhRJZ#JoR_uILrYfP30IhQ6r4sHi4KnPuU2NPWtToqm64v5gE", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
